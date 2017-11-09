d1fcddf7cbc387485464fa5e91b07843fec0a722
Ubuntu <ubuntu@ip-172-31-21-241.us-west-2.compute.internal>
2017-11-09 02:33:16 +0000

    Did check for if user doesn't have grid_out object

--- HEMSapp/views.py
+++ HEMSapp/views.py
@@ -9,18 +9,20 @@ def index(request):
 def home(request):
     first_name = request.user.first_name
     last_name = request.user.last_name
-
-    grid_out = GridOut.objects.filter(grid__owner=request.user)[0]
-    first_data = GridPowerNet.objects.filter(object_id=grid_out.unique_id,).first()
-    first_month = first_data.timestamp.strftime('%B %Y') # so we know where to start our chart
-    boxes = get_assets(request.user.hemsbox_set.all()) #func comes from models.py
-
     context = {
         'first_name': first_name,
         'last_name': last_name,
-        'first_month': first_month,
-        'boxes': boxes
-    }
+    } 
+    grid_out = GridOut.objects.filter(grid__owner=request.user)
+    if len(grid_out) > 0:    
+        grid_out = grid_out[0]
+	first_data = GridPowerNet.objects.filter(object_id=grid_out.unique_id,).first()
+    	first_month = first_data.timestamp.strftime('%B %Y') # so we know where to start our chart
+        context['first_month'] = first_month
+    boxes = get_assets(request.user.hemsbox_set.all()) #func comes from models.py
+ 
+    context['boxes'] = boxes
+   
 
     return render(request, 'HEMSapp/ratePayerDash.html', context)
 
