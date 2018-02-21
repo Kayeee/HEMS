c2be6b24f062f38ceafc0bae0a972aed23efc866
jessica <jessica@jessicas-air-3.mobile.asu.edu>
2018-02-21 14:32:10 -0700

    Fix custom element show and hide problem

--- HEMSapp/static/js/selectDevice.js
+++ HEMSapp/static/js/selectDevice.js
@@ -8,7 +8,7 @@ function check_field_value(){
 }
 
 $(document).ready(function(){
-  $('#hems_devices').change(function(){
-    check_field_value.call($('#hems_devices'));
+  $('div select').first().change(function(){
+    check_field_value.call($('div select').first());
   });
 });
