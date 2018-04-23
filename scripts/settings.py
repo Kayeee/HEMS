ec27eda25bec3ad163f725b857de3d2effeca495
jessica <jessica@jessicas-MacBook-Air-3.local>
2018-04-22 21:55:21 -0700

    Modified settings file

--- scripts/settings.py
+++ scripts/settings.py
@@ -140,8 +140,9 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/hems/static/'
+STATIC_ROOT=''
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
