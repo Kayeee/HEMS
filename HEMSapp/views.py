51008682e50009ebb9cc8f19f836d060aa6709e6
jessica <jessica@jessicas-MacBook-Air-3.local>
2018-04-22 21:17:46 -0700

    Fixed the graph in the dashboard

--- HEMSapp/views.py
+++ HEMSapp/views.py
@@ -25,7 +25,7 @@ def home(request):
     grid_out = GridOut.objects.filter(grid__owner=request.user)
     if len(grid_out) > 0:
         grid_out = grid_out[0]
-	first_data = GridPowerNet.objects.filter(object_id=grid_out.unique_id,).first()
+	first_data = GridPowerNet.objects.filter(object_id=grid_out.unique_id).first()
     	first_month = first_data.timestamp.strftime('%B %Y') # so we know where to start our chart
         context['first_month'] = first_month
     boxes = get_assets(request.user.hemsbox_set.all()) #func comes from models.py
