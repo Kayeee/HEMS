d8a8e7e50a70ce58aa24753c80a34848695af603
jessica <jessica@jessicas-MacBook-Air-3.local>
2018-04-22 21:46:00 -0700

    Modified settings file to load static files and media

--- scripts/settings.py
+++ scripts/settings.py
@@ -140,5 +140,8 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/hems/static/'
-STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
